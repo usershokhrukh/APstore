9e440a054d93a0a2b477f8e39f6831f1264e4d09
usershokhrukh <asxurovsit@gmail.com>
2026-07-25 15:02:07 +0500

    features/develop APstore file structure

---
+++ src/app/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./page.modules.scss";
+import Header from '@/components/header/Header';
+import Sidebar from '@/components/sidebar/Sidebar';
+import Dashboard from './dashboard/page';
+
+const Page = () => {
+  return (
+    <div className='page'>
+      <Header/>
+      <main className='page__main'>
+        <Sidebar/>
+        <Dashboard/>
+      </main>
+    </div>
+  )
+}
+
+export default Page
---
+++ src/app/products/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./products.modules.scss"
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
