1291e7173594cdc6665bde81a217b8247e8fae02
usershokhrukh <asxurovsit@gmail.com>
2026-07-26 17:27:24 +0500

    features/develop all configured

---
+++ src/app/api/auth/refresh/route.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import {cookies} from "next/headers";
+
+export async function POST() {
+  const cookieStore = await cookies();
+  const currentRefreshToken = cookieStore.get("refresh_token")?.value;
+
+  if (!currentRefreshToken) {
+    return Response.json({error: "Session non-existent"}, {status: 401});
+  }
+
+  try {
+    const backendResponse = await axios.post(
+      "https://api.magnateshop.uz/api/v1/auth/refresh",
+      {refreshToken: currentRefreshToken},
+    );
+    if (backendResponse?.data?.statusCode == 400) {
+      return Response.json({error: "Failed to get token!"}, {status: 400});
+    } else {
+      const data = await backendResponse.data;
+      cookieStore.set("access_token", data.accessToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        path: "/",
+        maxAge: 15 * 60,
+      });
+
+      cookieStore.set("refresh_token", data.refreshToken, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        path: "/api/auth/refresh",
+        maxAge: 7 * 24 * 60 * 60,
+      });
+
+      return Response.json({success: true});
+    }
+  } catch (err) {
+    cookieStore.delete("access_token", {path: "/"});
+    cookieStore.delete("refresh_token", {path: "/api/auth/refresh"});
+
+    return Response.json({error: "Invalid session credentials"}, {status: 401});
+  }
+}
--- src/hooks/login/PostLogin.js
+++ src/hooks/login/PostLogin.js
@@ -1,20 +1,27 @@
-"use client"
+"use client";
 
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-const { default: axios } = require("axios")
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import axios from "axios";
 
 const request = async (data) => {
-  const api = process.env.NEXT_PUBLIC_API_KEY;  
-  const res = await axios.post(`${api}/api/v1/auth/login`, data)
+  const api_key = process.env.NEXT_PUBLIC_API_KEY;
+  const res = await axios.post(`${api_key}/api/v1/auth/login`, data);
+  const {access_token, refresh_token} = res.data;
+
+  const cookieRes = await axios.post("/api/auth/login", {
+    accessToken: access_token,
+    refreshToken: refresh_token,
+  });
+
   return res;
-}
+};
 
 export const PostLogin = () => {
   const queryClient = useQueryClient();
   return useMutation({
     mutationFn: request,
     onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ["login"]})
-    }
-  })
-}
+      queryClient.invalidateQueries({queryKey: ["login"]});
+    },
+  });
+};
