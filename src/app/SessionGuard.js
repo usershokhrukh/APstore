47bdd97a1c453d479d7195515a7a85a0c7b7fc04
usershokhrukh <asxurovsit@gmail.com>
2026-07-30 02:50:42 +0500

    features/develop scss changed

--- src/app/SessionGuard.js
+++ src/app/SessionGuard.js
@@ -1,16 +1,18 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
 
 export default function SessionGuard() {
-  const router  = useRouter();
-  const first = sessionStorage.getItem("isFresh");
-  if (!first) {
-    const url = new URL(window.location.href);
-    url.searchParams.set("fresh_session", "true");
-    sessionStorage.setItem("isFresh", "true")
-    window.location.href = url.toString();
-    // router.push("/?fresh_session=false")
-  }
+  useEffect(() => {
+    const first = sessionStorage.getItem("isFresh");
+    if (!first) {
+      const url = new URL(window.location.href);
+      url.searchParams.set("fresh_session", "true");
+      sessionStorage.setItem("isFresh", "true");
+      window.location.href = url.toString();
+    }
+  }, []);
+
   return null;
 }
