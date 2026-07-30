b3a1472347bb64245246362a85ad5ba41151a417
elton-program <sevaramurshaeva86@gmail.com>
2026-07-30 18:39:07 +0500

    header upgrades

--- src/app/NotificationCustom.jsx
+++ src/app/NotificationCustom.jsx
@@ -1,9 +1,14 @@
 "use client";
 
-import React, {useEffect, useRef, useState} from "react";
-import {Providers} from "./provides";
-import {ErrorContext} from "@/context/ErrorContext";
-const NotificationCustom = ({children}) => {
+import React, { useEffect, useRef, useState } from "react";
+import { Providers } from "./provides";
+import { StatusContext } from "@/context/StatusContext";
+import { ErrorContext } from "@/context/ErrorContext";
+const NotificationCustom = ({ children }) => {
+  const [status, setStatus] = useState({
+    text: "",
+    type: "",
+  });
   const [error, setError] = useState({
     text: "",
     timeout: 5000,
@@ -51,34 +56,36 @@ const NotificationCustom = ({children}) => {
     setErrorClose(null);
   };
   return (
-    <ErrorContext.Provider value={[setError, setNoticeSVG]}>
-      <Providers>
-        <span
-          className={`error__span-wrap ${errorClose ? "animate-notify" : errorClose == false ? "error__none" : "animate-notify-close"}`}
-        >
-          <span className="error__span">
-            {noticeSVG}
-            <span className="error__span-text">{error.text}</span>
+    <StatusContext.Provider value={[ status, setStatus ]}>
+      <ErrorContext.Provider value={[setError, setNoticeSVG]}>
+        <Providers>
+          <span
+            className={`error__span-wrap ${errorClose ? "animate-notify" : errorClose == false ? "error__none" : "animate-notify-close"}`}
+          >
+            <span className="error__span">
+              {noticeSVG}
+              <span className="error__span-text">{error.text}</span>
 
-            {error?.close ? (
-              <span
-                onClick={handleNotice}
-                className={`error__close ${errorClose ? "animate-close" : ""}`}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
+              {error?.close ? (
+                <span
+                  onClick={handleNotice}
+                  className={`error__close ${errorClose ? "animate-close" : ""}`}
                 >
-                  <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z"></path>
-                </svg>
-              </span>
-            ) : null}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                  >
+                    <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 10.5858L9.17157 7.75736L7.75736 9.17157L10.5858 12L7.75736 14.8284L9.17157 16.2426L12 13.4142L14.8284 16.2426L16.2426 14.8284L13.4142 12L16.2426 9.17157L14.8284 7.75736L12 10.5858Z"></path>
+                  </svg>
+                </span>
+              ) : null}
+            </span>
           </span>
-        </span>
-        {children}
-      </Providers>
-    </ErrorContext.Provider>
+          {children}
+        </Providers>
+      </ErrorContext.Provider>
+    </StatusContext.Provider>
   );
 };
 
