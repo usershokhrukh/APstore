6126bf6337e4c2e671af52fa221d61b4e3e6e3f7
elton-program <sevaramurshaeva86@gmail.com>
2026-08-02 22:22:57 +0500

    some changes

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -33,7 +33,7 @@ const page = () => {
         </thead>
         <tbody>
           {products?.length > 0 ? (
-            products.map((product,index) => (
+            products.map((product, index) => (
               <tr key={product.id}>
                 <td>{index + 1}</td>
                 <td>
@@ -56,10 +56,26 @@ const page = () => {
                 <td>{product.stock} ta</td>
                 <td className="product__block">
                   <button className="product__edit" onClick={() => {}}>
-                    Edit
+                    <span className="product__span">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M6.41421 15.89L16.5563 5.74785L15.1421 4.33363L5 14.4758V15.89H6.41421ZM7.24264 17.89H3V13.6473L14.435 2.21231C14.8256 1.82179 15.4587 1.82179 15.8492 2.21231L18.6777 5.04074C19.0682 5.43126 19.0682 6.06443 18.6777 6.45495L7.24264 17.89ZM3 19.89H21V21.89H3V19.89Z"></path>
+                      </svg>
+                    </span>
                   </button>
                   <button className="product__delete" onClick={() => {}}>
-                    Delete
+                    <span className="product__span">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                      >
+                        <path d="M4 8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8ZM6 10V20H18V10H6ZM9 12H11V18H9V12ZM13 12H15V18H13V12ZM7 5V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V5H22V7H2V5H7ZM9 4V5H15V4H9Z"></path>
+                      </svg>
+                    </span>
                   </button>
                 </td>
               </tr>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,10 +3,15 @@
 import React, { useContext, useState } from "react";
 import "./header.modules.scss";
 import { StatusContext } from "@/context/StatusContext";
+import { usePathname } from "next/navigation";
 const Header = () => {
   const [status, setStatus] = useContext(StatusContext)  
+  const pathname = usePathname();
   return (
     <div className="header">
+      <div className="header__path">
+        <p className="header__title">{pathname == "/products"? ("Product"):(null)}</p>
+      </div>
       <div className="header__status">
         <span >{status.text}</span>
         <span className={`header__dot header__dot--${status.type}`} />
