f7aa00370efaac5a29c2537b998e4156db84068e
elton-program <sevaramurshaeva86@gmail.com>
2026-08-03 13:49:35 +0500

    method Delete

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -1,11 +1,16 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./products.modules.scss";
-import { useGetProducts } from "@/hooks/products/GetProducts";
+import {
+  useGetProducts,
+  useDeleteProducts,
+} from "@/hooks/products/GetProducts";
 import { useStatus } from "@/hooks/useStatus";
 const page = () => {
   const { processStatus } = useStatus();
   const { data: products, isLoading, isError, error } = useGetProducts();
+  const { mutate: deleteProduct } = useDeleteProducts();
+  const [value, setValue] = useState("");
   useEffect(() => {
     if (isLoading) {
       processStatus("Loading...", "pending");
@@ -15,9 +20,57 @@ const page = () => {
       processStatus("Success!", "fulfilled");
     }
   }, [isLoading, isError, products?.length]);
-
+  const handleDelete = (id) => {
+    if (window.confirm("Rostdan ham ushbu mahsulotni o'chirmoqchimisiz?")) {
+      deleteProduct(id, {
+        onSuccess: () => {
+          processStatus("Success!", "fulfilled");
+        },
+        onError: (err) => {
+          processStatus(err?.message || "O'chirishda xatolik bo'ldi", "reject");
+        },
+      });
+    }
+  };
   return (
     <div className="product">
+      <div className="product__header">
+        <div className="search__container">
+          <input
+            type="text"
+            placeholder="Search"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            className="search-input"
+          />
+
+          {value ? (
+            <span
+              className="search-icon clear-icon"
+              onClick={() => setValue("")}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M21.7782 4H18.9411L14.4519 8.37097C13.7983 9.00497 12.9175 9.36038 11.9998 9.36038C11.0822 9.36038 10.2014 9.00497 9.54783 8.37097L5.06209 4H2.22266L8.12927 9.75399C10.269 11.8366 13.7357 11.8366 15.8739 9.75399L21.7782 4ZM2.22266 20H5.05095L9.55942 15.6151C10.2105 14.9842 11.088 14.6305 12.0021 14.6305C12.9163 14.6305 13.7938 14.9842 14.4449 15.6151L18.9518 20H21.7782L15.8581 14.2388C13.7264 12.1663 10.2729 12.1663 8.14279 14.2388L2.22266 20Z"></path>
+              </svg>
+            </span>
+          ) : (
+            <span className="search-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path>
+              </svg>
+            </span>
+          )}
+        </div>
+        <button className="product__add">Add Product</button>
+      </div>
       <table className="product__table">
         <thead>
           <tr>
@@ -66,7 +119,10 @@ const page = () => {
                       </svg>
                     </span>
                   </button>
-                  <button className="product__delete" onClick={() => {}}>
+                  <button
+                    className="product__delete"
+                    onClick={() => handleDelete(product.id)}
+                  >
                     <span className="product__span">
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
